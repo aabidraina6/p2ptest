44b74f7dc76d5c2787f40b131e1883f07c79d705
YashK2003 <102593613+YashK2003@users.noreply.github.com>
2023-03-06 16:28:50 +0530

    added the home page

--- src/App.js
+++ src/App.js
@@ -1,21 +1,6 @@
 import React from "react";
-// import { BrowserRouter as Router, Route} from "react-router-dom";
 import { Routes, Route, BrowserRouter } from 'react-router-dom'
-import { Mypage } from "./components/mypage";
-
-
-// function App()
-// {
-  
-//   return (
-//     <Router>
-//         <Route exact path="/" element={mypage} />
-//         <Route exact path="/home" element={mypage} />
-//     </Router>
-//   );
-
-// }
-
+import { Homepage } from "./components/homepage";
 
 function App()
 {
@@ -23,8 +8,8 @@ function App()
   return (
     <BrowserRouter>
       <Routes>
-        <Route  path="/" exact element={<Mypage />} />
-        <Route  path="/home" exact element={<Mypage />} />
+        <Route  path="/" exact element={<Homepage />} />
+        <Route  path="/home" exact element={<Homepage />} />
         </Routes>
     </BrowserRouter>
   );
