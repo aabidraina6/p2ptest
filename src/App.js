b5167a19640444b2989edc0bd869670bd24c3e34
Sankalp Sanjay Bahad <95967963+SankalpBahad@users.noreply.github.com>
2023-03-07 17:31:03 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Routes, Route, BrowserRouter } from 'react-router-dom'
 import { Homepage } from "./components/homepage";
+import { LoginPage } from "./components/login";
 
 function App()
 {
@@ -10,6 +11,7 @@ function App()
       <Routes>
         <Route  path="/" exact element={<Homepage />} />
         <Route  path="/home" exact element={<Homepage />} />
+        <Route  path="/login" exact element={<LoginPage />} />
         </Routes>
     </BrowserRouter>
   );
