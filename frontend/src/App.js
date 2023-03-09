7116eecfb2adf1874db34df04b95fb007cbacfd0
Abhinay Maurya <102323440+abhinay-301@users.noreply.github.com>
2023-03-09 23:26:44 +0530

    Add files via upload

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Routes, Route, BrowserRouter } from 'react-router-dom'
+import FrontPage from "./components/firstpage";
 import { Homepage } from "./components/homepage";
 import { LoginPage } from "./components/loginpage";
 import { RegistrationPage } from "./components/registrationpage";
@@ -10,7 +11,7 @@ function App()
   return (
     <BrowserRouter>
       <Routes>
-        <Route  path="/" exact element={<Homepage />} />
+        <Route  path="/" exact element={<FrontPage />} />
         <Route  path="/home" exact element={<Homepage />} />
         <Route  path="/login" exact element={<LoginPage />} />
         <Route path="/register" exact element={<RegistrationPage/>}></Route>
