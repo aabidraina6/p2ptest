493cf021231113f9fc9f486fb14cbe5ad8877e88
YashK2003 <102593613+YashK2003@users.noreply.github.com>
2023-03-12 22:17:18 +0530

    chat bug fix left

--- socket/server.js
+++ socket/server.js
@@ -31,15 +31,16 @@ io.on("connection", (socket) => {
         io.emit("getUsers", users);
     });
 
-    //     //send and get message
-    //     socket.on("sendMessage", ({ senderId, receiverId, text }) => {
-    //         const user = getUser(receiverId);
-    //         io.to(user.socketId).emit("getMessage", {
-    //             senderId,
-    //             text,
-    //         });
-    //     });
+    //send and get message
+    socket.on("sendMessage", ({ senderId, receiverId, text }) => {
+        const user = getUser(receiverId);
+        io.to(user.socketId).emit("getMessage", {
+            senderId,
+            text,
+        });
+    });
 
+    
     //when disconnect
     socket.on("disconnect", () => {
         console.log("a user disconnected!");
