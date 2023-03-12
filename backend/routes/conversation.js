3364074160f58db7321e7b0d32141d5c9e003d6c
YashK2003 <102593613+YashK2003@users.noreply.github.com>
2023-03-12 22:16:10 +0530

    chat bug fix left

--- backend/routes/conversation.js
+++ backend/routes/conversation.js
@@ -25,7 +25,7 @@ router.post("/", async (req, res) => {
 //get conv of a user
 
 router.post("/getconv", async (req, res) => {
-   
+
     console.log(req.body.userid);
     try {
         const conversation = await Conversation.find({
@@ -36,6 +36,27 @@ router.post("/getconv", async (req, res) => {
         res.status(500).json(err);
     }
 });
+
+
+// get the conversation based on id
+
+router.post("/togetrec", (req, res) => {
+    // console.log("reached the getuserdata function here");
+    console.log("body is here in mongodb ", req.body.cnid);
+
+    Conversation.find({ _id: req.body.cnid }, function (err, docs) {
+        if (err) {
+            console.log(err);
+            res.status(400);
+        }
+        else {
+            // console.log("First function call : ", docs);
+            // console.log(typeof(docs));
+            res.status(200).json(docs);
+        }
+    });
+}
+);
 
 
 module.exports = router;
